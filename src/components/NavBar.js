de43775c774b107756c3f24410538c32f2190362
Cezary Kocinski <ckocinski7@wp.pl>
2021-06-12 13:52:13 +0200

    fix class toggle issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-scroll'
 import './styles/NavBar.scss'
 
 const Navbar = () => {
-
+    const navbar = document.getElementById('navbar');
     const[isScrolled, setIsScrolled] = useState(false);
 
     const handleScroll = () => {
@@ -17,8 +17,12 @@ const Navbar = () => {
     window.addEventListener('scroll', handleScroll);
 
     const toggle = () => {
-        const navbar = document.getElementById('navbar');
         navbar.classList.toggle('toggle');
+    }
+    const closeNavbar = () => {
+        if(navbar.classList.contains('toggle')) {
+            navbar.classList.remove('toggle');
+        }
     }
 
 
@@ -29,11 +33,11 @@ const Navbar = () => {
             </div>
             <nav>
                 <ul className="navigation">
-                    <li><Link onClick={toggle} activeClass="active" to="intro" smooth={true}><span>Home</span></Link></li>
-                    <li><Link  onClick={toggle}to="skills" spy={true} smooth={true}><span>Skills</span></Link></li>
-                    <li><Link onClick={toggle} to="projects" spy={true} smooth={true}><span>Projects</span></Link> </li>
-                    <li><Link onClick={toggle} to="about" spy={true} smooth={true}><span>About</span></Link></li>
-                    <li><Link onClick={toggle} to="contact" spy={true} smooth={true}><span>Contact</span></Link></li>
+                    <li><Link onClick={closeNavbar} activeClass="active" to="intro" smooth={true}><span>Home</span></Link></li>
+                    <li><Link onClick={closeNavbar}to="skills" spy={true} smooth={true}><span>Skills</span></Link></li>
+                    <li><Link onClick={closeNavbar} to="projects" spy={true} smooth={true}><span>Projects</span></Link> </li>
+                    <li><Link onClick={closeNavbar} to="about" spy={true} smooth={true}><span>About</span></Link></li>
+                    <li><Link onClick={closeNavbar} to="contact" spy={true} smooth={true}><span>Contact</span></Link></li>
                 </ul>
             </nav>
             <div onClick={toggle} className="toggle-btn">
