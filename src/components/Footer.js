986f04fcb763572b25be428d985d6617fa65c857
Cezary Kocinski <ckocinski7@wp.pl>
2021-06-26 13:17:16 +0200

    update social media

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
                 <a href="https://github.com/cyq7" target="_blank" rel="noreferrer"><i className="fab fa-github icon"></i></a>
                 <a href="https://pl.linkedin.com/in/cezary-kociński-1511a41b6"
                 target = "_blank"
-                rel = "noreferrer" > < i className = "fab fa-linkedin-in icon" > < /i></a >
+                rel = "noreferrer" > <i className = "fab fa-linkedin-in icon"> </i></a>
                 <a href="https://www.facebook.com/czarek.kocinski" target="_blank" rel="noreferrer"><i className="fab fa-facebook-f icon"></i></a>
                 </div>
             <p>Copyright © 2021 Cezary Kociński</p>
